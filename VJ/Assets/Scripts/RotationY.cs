cc60529564bf34dfd7a8082e36f0f3df1daa7d7a
Keichi Takahashi <keichi.t@me.com>
2014-04-13 16:25:07 +0900

    Last fix

---
+++ VJ/Assets/Scripts/RotationY.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationY : MonoBehaviour {
+
+	public float angularVelocity = 45;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(transform.right, angularVelocity*Time.deltaTime);
+	}
+}
