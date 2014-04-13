c715c4247e36c1c82999723e41ac7d179028c6af
tchacksonps4 <peugeot.chat.pied@gmail.com>
2014-04-13 13:53:09 +0900

    早送り機能追加

--- VJ/Assets/Scripts/AudioManager.cs
+++ VJ/Assets/Scripts/AudioManager.cs
@@ -5,14 +5,25 @@ public class AudioManager : MonoBehaviour
 {
 	public AudioSource audioSource;
 	public float audioTime = 0.0f;
+	public float CurrentAudioTime = 1.0f;
+
 	// Use this for initialization
 	void Start ()
 	{
-	
 	}
 	// Update is called once per frame
 	void Update ()
 	{
 		audioTime = audioSource.time;
+
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			audioSource.time = audioTime - CurrentAudioTime;
+		}
+
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			audioSource.time = audioTime + CurrentAudioTime;
+		}
 	}
 }
