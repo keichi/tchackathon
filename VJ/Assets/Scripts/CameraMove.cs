c5de20fb3e4f401ed1adb56caa3320a2bc2f4e0e
tchacksonps4 <peugeot.chat.pied@gmail.com>
2014-04-13 15:31:56 +0900

    エフェクト修正

---
+++ VJ/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+	public GameObject camera;
+	public Vector3 endPos;
+
+	public VJGameObjectTrigger voice;
+
+	public bool isPause = false;
+
+	// Use this for initialization
+	void Start () {
+		endPos = camera.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPause) {
+			return;
+		}
+		Vector3 pos = endPos;
+		pos.z += (3.0f - voice.lastReturnedValue * 3.0f);
+
+		camera.transform.localPosition -= (camera.transform.localPosition - pos) * 0.1f;
+	}
+}
--- VJ/Assets/Scripts/VJSequencer.cs
+++ VJ/Assets/Scripts/VJSequencer.cs
@@ -7,36 +7,36 @@ public class VJSequencer : MonoBehaviour
 	public GameObject[] Effects = new GameObject[4];
 	private int currentEffectIndex = 0;
 	private Dictionary<string, int> MOOD_DIC = new Dictionary<string, int> () {
-		{ "Energizing",0 },
-		{ "Sensual",1 },
-		{ "Defiant",2 },
+		{ "Energizing",2 },
+		{ "Sensual",0 },
+		{ "Defiant",3 },
 		{ "Easygoing",3 },
-		{ "Cool",0 },
-		{ "Urgent",1 },
-		{ "Excited",2 },
+		{ "Cool",2 },
+		{ "Urgent",2 },
+		{ "Excited",3 },
 		{ "Sophisticated",3 },
-		{ "Fiery",0 },
-		{ "Melancholy",1 },
+		{ "Fiery",2 },
+		{ "Melancholy",2 },
 		{ "Somber",2 },
 		{ "Other",3 },
-		{ "Fun",0 },
-		{ "Tender",1 },
-		{ "Blue",2 },
-		{ "Sentimental",3 },
-		{ "Peaceful",0 },
-		{ "Yearning",1 },
+		{ "Fun",2 },
+		{ "Tender",2 },
+		{ "Blue",3 },
+		{ "Sentimental",2 },
+		{ "Peaceful",2 },
+		{ "Yearning",3 },
 		{ "Romantic",2 },
 		{ "Stirring",3 },
-		{ "Brooding",0 },
-		{ "Empowering",1 },
-		{ "Rowdy",2 },
+		{ "Brooding",2 },
+		{ "Empowering",2 },
+		{ "Rowdy",1 },
 		{ "Lively",3 },
-		{ "Aggressive",0 },
+		{ "Aggressive",2 },
 	};
 	// Use this for initialization
 	void Start ()
 	{
-		Effects [currentEffectIndex].SetActive (true);
+		//Effects [currentEffectIndex].SetActive (true);
 	}
 	// Update is called once per frame
 	void Update ()
@@ -48,6 +48,8 @@ public class VJSequencer : MonoBehaviour
 		int index = 0;
 		if (MOOD_DIC.TryGetValue (mood, out index)) {
 			ChangeEffect (MOOD_DIC [mood]);
+			var camera = gameObject.GetComponent<CameraMove> ();
+			camera.isPause = mood.Equals("Sensual");
 		}
 	}
 
